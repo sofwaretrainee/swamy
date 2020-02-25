b73fddf20f5fe299f0cfcef5c27d8bef0fd6766e
Swamymr <swamy_mr@ndvor.com>
2020-02-25 18:38:09 +0530

    WebApi

---
+++ WebApi/MyWebApi/DatabaseLayer/IDatabaseHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace Factory_Pattern16
+{
+   interface IDatabaseHandler
+    {
+        IDbConnection CreateConnection();
+
+        void CloseConnection(IDbConnection connection);
+
+        IDbCommand CreateCommand(string commandText, CommandType commandType, IDbConnection connection);
+
+        IDataAdapter CreateAdapter(IDbCommand command);
+
+        IDbDataParameter CreateParameter(IDbCommand command);
+    }
+}
